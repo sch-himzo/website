4406194bec5b33aa684a19aeb485cf5faa2df7b4
Benedek Burgess <benedekb97@gail.com>
2019-11-12 17:58:25 +0100

    VIewport

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
         <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
         <script src="https://kit.fontawesome.com/492a2c0a6b.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
     @include('layouts.nav')
