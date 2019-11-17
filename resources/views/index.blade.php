759caed24771bac80cfe6c3d6ad66441feeb2fb2
Benedek Burgess <benedekb97@gail.com>
2019-11-17 14:57:42 +0100

    Fix front page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,6 +30,7 @@
         <ol class="carousel-indicators">
             <li data-target="carousel" data-slide-to="0" class="active"></li>
             <li data-target="carousel" data-slide-to="1"></li>
+            <li data-target="carousel" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
             <div class="item active first-slide">
@@ -57,12 +58,12 @@
                 </div>
             </div>
         </div>
-        <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <a style="line-height:500px;" class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+            <i class="fa fa-chevron-left"></i>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <a style="line-height:500px;" class="right carousel-control" href="#carousel" role="button" data-slide="next">
+            <i class="fa fa-chevron-right"></i>
             <span class="sr-only">Next</span>
         </a>
     </div>
