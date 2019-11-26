94bee9e3da952261d7aaeb02a319f269ee3353f2
Benedek Burgess <benedekb97@gail.com>
2019-11-26 17:14:15 +0100

    Can view price and design file from my orders page

--- resources/views/user/orders.blade.php
+++ resources/views/user/orders.blade.php
@@ -15,10 +15,11 @@
                             <th>Cím</th>
                             <th>Leadva</th>
                             <th>Határidő</th>
-                            <th>Kép</th>
+                            <th>Képek</th>
                             <th>Darabszám</th>
                             <th>Állapot</th>
                             <th>Képek</th>
+                            <th>Ár</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -38,6 +39,13 @@
                                             <i class="fa fa-image"></i>
                                         </a>
                                     </span>
+                                    @if($order->getDST()!=null)
+                                        <span data-toggle="tooltip" title="Elkészült tervfájl megtekintése">
+                                            <button type="button" data-toggle="modal" data-target="#svg_{{ $order->id }}" class="btn btn-xs btn-primary">
+                                                <i class="fa fa-pencil"></i>
+                                            </button>
+                                        </span>
+                                    @endif
                                 </td>
                                 <td>{{ $order->count }}</td>
                                 <td>{{ $order->getStatusClient() }}</td>
@@ -50,10 +58,38 @@
                                         <i>Nincs feltöltött kép</i>
                                     @endif
                                 </td>
+                                @if($order->getCost()!=null)
+                                    <td>@if($order->original) {{ $order->getDesignCost() }} + @endif {{ $order->count }} &times; {{ $order->getCost() }} = {{ number_format($order->getTotalCost(),0,',','.') }} Ft</td>
+                                @else
+                                    <td><i>Nem készült még el</i></td>
+                                @endif
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
+@endsection
+
+@section('modals')
+    @foreach($orders as $order)
+        @if($order->getDST()!=null)
+            <div class="modal fade" id="svg_{{ $order->id }}">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button class="close" type="button" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Tervrajz megtekintése</h4>
+                        </div>
+                        <div class="modal-body">
+                            <img class="album-edit-image" src="{{ asset('storage/images/svg/'. $order->getDST()->svg) }}" alt="{{ $order->title }}">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Bezárás</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+    @endforeach
 @endsection
