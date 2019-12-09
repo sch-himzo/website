43d9965c4dee99a8b6efd08dea67fd46e638ecfe
Benedek Burgess <benedekb97@gail.com>
2019-12-09 19:00:21 +0100

    fix ping email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function(){
             EmailController::sendPings();
-        })->everyMinute();
+        })->dailyAt('8:00');
     }
 
     /**
