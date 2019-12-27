35961e94473d216f4e0af5a6ac8d182c3cae56fd
Benedek Burgess <benedekb97@gail.com>
2019-12-27 11:48:22 +0100

    log everything

--- resources/views/machines/status.blade.php
+++ resources/views/machines/status.blade.php
@@ -75,6 +75,7 @@
                     if('new_design' in e){
                         document.location.reload();
                     }else{
+                        console.log(e);
                         if(current_state!==e.state){
                             $('#state').html(e.status);
                             $('#current_state').val(e.state);
