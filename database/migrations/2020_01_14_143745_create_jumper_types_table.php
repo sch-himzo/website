e1a3803cfcc24313484c5d5b18c0b922c5903509
Benedek Burgess <benedekb97@gail.com>
2020-01-14 17:14:22 +0100

    Fix oopsie

--- database/migrations/2020_01_14_143745_create_jumper_types_table.php
+++ database/migrations/2020_01_14_143745_create_jumper_types_table.php
@@ -16,7 +16,7 @@ class CreateJumperTypesTable extends Migration
         Schema::create('jumper_types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->enum('size',['XS','S','M','L','XL','XXL','3XL','4XL','5XL']);
-            $table->enum('color', ['original','blue','black','red','yellow','white','green']);
+            $table->enum('color', ['original','blue','black','red','yellow','white','green','grey','black-grey']);
             $table->unsignedInteger('count');
             $table->timestamps();
         });
