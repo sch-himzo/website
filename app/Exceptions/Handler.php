6d5589067ab17a754df6ba0b257faffc52504e36
Burgess Benedek <burgess.benedek@straxus.hu>
2020-10-29 19:04:36 +0100

    remove old issue collector

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,38 +51,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        $message = $exception->getMessage();
-        $file = $exception->getFile();
-        $code = $exception->getCode();
-        $user = Auth::check() ? Auth::user()->name : null;
-        $url = $request->fullUrl();
-        $token = env("SLACK_TOKEN");
-        $stack_trace = $exception->getTraceAsString();
-        $type = get_class($exception);
-
-        $exclude = [
-            'Symfony\Component\HttpKernel\Exception\NotFoundHttpException',
-            'Symfony\Component\HttpKernel\Exception\HttpException'
-
-        ];
-
-        if(!in_array($type, $exclude) && $message != 'CSRF token mismatch.') {
-            $c = curl_init();
-
-            curl_setopt($c, CURLOPT_URL, "https://hooks.slack.com/services/T6VJX9GAX/BSY1E5WF9/$token");
-            curl_setopt($c, CURLOPT_POST, 1);
-            curl_setopt($c, CURLOPT_POSTFIELDS, json_encode([
-                'text' => "Code: $code\nIn File: $file\nOn Page: $url\nUser: $user\n\nMessage: $message\nStack trace:\n$stack_trace"
-            ]));
-            curl_setopt($c, CURLOPT_HEADER, [
-                "Content-type" => "application/json"
-            ]);
-
-            curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-
-            curl_exec($c);
-        }
-
         return parent::render($request, $exception);
     }
 }
