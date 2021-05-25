3bbb8abd89701dff2b61ba2d236c6c2c06594fe8
Benedek Burgess <benedekb97@gmail.com>
2021-05-25 13:12:34 +0200

    Update packages

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        LaravelDoctrine\ORM\DoctrineServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -176,7 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Sentry\Laravel\ServiceProvider::class,
+        \Sentry\Laravel\ServiceProvider::class,
     ],
 
     /*
